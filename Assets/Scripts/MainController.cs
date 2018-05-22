068c00aca8e6f7c976fdd34c3243e8b0e3fa1958
hayu <38928136+hayugub@users.noreply.github.com>
2018-05-22 23:56:21 +0900

    プロジェクト中身の追加

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MainController : MonoBehaviour {
+	
+//	[SerializeField] GameObject[] Nekos;
+	[SerializeField] GameObject objField;
+	[SerializeField] Text textScore;
+	[SerializeField] int nekoNum;
+	[SerializeField] BoxCollider2D filter;
+	[SerializeField] GameObject objGameOver;
+
+	GameObject nekoPrefab;
+	int score = 0;
+	float filterOffsetY;
+	bool down = false;
+	int point = 0;
+
+	// Use this for initialization
+	void Start () {
+
+		if (nekoPrefab == null)
+			return;
+
+		Init ();
+	}
+
+	void Init () {
+		filterOffsetY = -1f;
+		point = 0;
+		objGameOver.SetActive (false);
+
+		foreach(Transform child in objField.gameObject.transform){
+			Destroy(child.gameObject);
+		}
+
+		for (int i = 0; i < nekoNum; i++) {
+			var nekoObj = Instantiate (nekoPrefab, objField.gameObject.transform);
+			var neko = nekoObj.GetComponent<NekoController> ();
+			neko.Init (this);
+		}		
+	}
+		
+	void Awake () {
+		LoadResouces ();
+	}
+
+	void LoadResouces () {
+		nekoPrefab = (GameObject)Resources.Load ("Prefabs/Neko");
+	}
+
+	public void AddNeko () {
+		var nekoObj = Instantiate (nekoPrefab, objField.gameObject.transform);
+		var neko = nekoObj.GetComponent<NekoController> ();
+		neko.Init (this);
+	}
+
+	public void SetPoint (int point) {
+		this.point = point;
+	}
+
+	void Update () {
+		if (filterOffsetY > 1.5f) {
+			objGameOver.SetActive (true);
+
+//			Init ();
+			return;
+		}
+
+		if (point == 0) {
+			filterOffsetY += 0.01f;
+		} else {
+			filterOffsetY += point * -0.1f;		
+		}
+
+		filter.transform.position = new Vector3 (filter.transform.position.x, filterOffsetY, filter.transform.position.z);
+	
+		point = 0;
+	}
+}
---
+++ Assets/Scripts/NekoController.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class NekoController : MonoBehaviour {
+	MainController mainController;
+	[SerializeField] GameObject[] nekos;
+	[SerializeField] Rigidbody2D rigidbody;
+	[SerializeField] BoxCollider2D box;
+	[SerializeField] CircleCollider2D circle;
+	bool isShow = false;
+	bool isBingo = false;
+
+	enum NekoType {
+		Neko = 0,
+		Inu,
+		NekoRainbow,
+	}
+
+	NekoType nekoType;
+
+
+	public void Init (MainController parent) {
+		mainController = parent;
+		ToggleNeko ();
+	}
+
+	void ToggleNeko () {
+		int index = 0;
+		index = UnityEngine.Random.Range (0, 20);
+		if (index == 10)
+			index = (int)NekoType.NekoRainbow;
+		else
+			index = index % 2;
+		SwitchNeko (index);
+	}
+
+	void ToggleNeko (NekoType type) {
+		SwitchNeko ((int)type);
+	}
+
+	void SwitchNeko (int index) {
+		nekoType = (NekoType)index;
+		for (int i = 0; i < nekos.Length; i++) {
+			if (i == index) {
+				nekos[i].SetActive (true);
+				continue;
+			}
+			nekos[i].SetActive (false);
+		}		
+	}
+
+	void Start () {
+	}
+
+	void Awake () {
+	}
+
+	private IEnumerator DelayMethod(float waitTime, Action action)
+	{
+		yield return new WaitForSeconds(waitTime);
+		action();
+	}
+
+	private Vector3 screenPoint;
+	private Vector3 offset;
+
+	void OnMouseDown ()
+	{
+		iTween.PunchScale (this.gameObject, iTween.Hash (
+			"x", 0.3f,
+			"y", 0.3f,
+			"time", 0.5
+		));
+
+//		 マウスカーソルは、スクリーン座標なので、
+//		 対象のオブジェクトもスクリーン座標に変換してから計算する。
+
+//		 このオブジェクトの位置(transform.position)をスクリーン座標に変換。
+		screenPoint = Camera.main.WorldToScreenPoint (transform.position);
+//		 ワールド座標上の、マウスカーソルと、対象の位置の差分。
+		offset = transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+		SwitchEnableCollider (false);
+	}
+
+	void OnMouseDrag ()
+	{
+		Vector3 currentScreenPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		Vector3 currentPosition = Camera.main.ScreenToWorldPoint (currentScreenPoint) + this.offset;
+		transform.position = currentPosition;
+	}
+
+	void OnMouseUp () {
+		iTween.PunchScale (this.gameObject, iTween.Hash (
+			"x", 0.3f,
+			"y", 0.3f,
+			"time", 0.5
+		));
+		SwitchEnableCollider (true);
+		rigidbody.AddForce (Vector3.up * 100);
+
+	}
+
+	void SwitchEnableCollider (bool enable) {
+		box.enabled = enable;
+		circle.enabled = enable;
+	}
+
+	void Update () {
+		if (transform.position.y > 6) {
+			//猫サイドに投げられた時
+			int point = 0;
+			if (transform.position.x > 0) {
+				switch (nekoType) {
+				case NekoType.Neko:
+					point = 1;
+					break;
+				case NekoType.NekoRainbow:
+					point = 3;
+					break;
+				case NekoType.Inu:
+					point = -1;
+					break;
+				default:
+					break;
+				}
+			}
+
+			else {
+				switch (nekoType) {
+				case NekoType.Neko:
+					point = -1;
+					break;
+				case NekoType.NekoRainbow:
+					point = -5;
+					break;
+				case NekoType.Inu:
+					point = 1;
+					break;
+				default:
+					break;
+				}
+			}
+
+			mainController.SetPoint (point);
+			mainController.AddNeko ();
+			Destroy (this.gameObject);
+		}
+	}
+		
+}
