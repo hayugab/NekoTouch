3a31a318ee3bbbda41ad0d38e751e93439a8df57
hayu <38928136+hayugub@users.noreply.github.com>
2018-06-05 18:28:34 +0900

    tutorialの猫タップ動作を追加

---
+++ Assets/Scripts/TutorialNekoController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialNekoController : MonoBehaviour {
+
+	[SerializeField] AudioSource audio;
+
+	// Use this for initialization
+	void OnMouseUp () {
+		audio.Play ();
+		iTween.PunchScale (this.gameObject, iTween.Hash (
+			"x", 0.3f,
+			"y", 0.3f,
+			"time", 0.5
+		));	
+	}
+}
